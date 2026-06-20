51d8c989938729a6bba9972525253428f7dd2488
rahul raj <thisisrajtkd18@gmail.com>
2026-06-20 17:22:37 +0530

    changing the frontend components

--- frontend/instantPost/src/pages/CreatePost.jsx
+++ frontend/instantPost/src/pages/CreatePost.jsx
@@ -23,16 +23,17 @@ const CreatePost = () => {
     }
     return (
 
-        <div>
+        <div className='max-w-[400px] h-[400px] mx-auto mt-[180px] border-2 border-black rounded-md px-[30px] py-[30px] flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 '>
             {
                 loading ? (<Spinner/>) :
-                    (<section className='max-w-2xl h-[520px] mx-auto mt-[60px] border-2 border-black rounded-md px-[50px] py-[60px]  bg-yellow-300 '>
-                        <h1 className='my-[30px] text-[20px] font-bold'>Create post</h1>
+                    (<section className=' w-[300px] h-[300px]  p-[20px] border-2 border-gray-500   rounded-lg bg-gradient-to-br 
+                    from-orange-400 to-red-300 '>
+                        <h1 className='my-[30px] text-[20px] font-bold text-center'>Create post</h1>
                         <form className='flex flex-col space-y-[20px]' onSubmit={handleFuction}>
                             {/* image and caption are the schema that we use in the model  */}
-                            <input type="file" name="image" accept="image/*"></input>
-                            <input type="text" name="caption" placeholder='enter caption'></input>
-                            <button className='border-2 border-gray-500 rounded-lg font-extrabold' type='submit'>SUBMIT</button>
+                            <input   type="file" name="image" accept="image/*"></input>
+                            <input className='rounded-md h-[30px] w-[250px] p-[10px] ' type="text" name="caption" placeholder='enter caption'></input>
+                            <button className='w-[250px] border-2  bg-green-400 rounded-lg font-extrabold hover:bg-blue-400' type='submit'>SUBMIT</button>
                         </form>
                     </section>)
             }
