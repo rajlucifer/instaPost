b365d545f701358039c7bdafb48f6e3567845d59
rahul raj <thisisrajtkd18@gmail.com>
2026-06-22 19:10:08 +0530

    DB try and catch included for better response

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 
-async  function connectDB(){
-    await mongoose.connect(process.env.DATABASE_URL);
-    console.log("db connected successfully");
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("DB connected successfully");
+    }
+    catch(error){
+        console.log("DB connection failed ");
+
+    }
 
 }
 
