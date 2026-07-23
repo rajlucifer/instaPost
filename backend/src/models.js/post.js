fcbd04ef1f1eb1e3889b40b55445771f0c1bb3d0
rahul raj <thisisrajtkd18@gmail.com>
2026-07-23 17:57:13 +0530

    feat(model): add shares field to Post schema

--- backend/src/models.js/post.js
+++ backend/src/models.js/post.js
@@ -12,6 +12,7 @@ const postSchema =  new mongoose.Schema({
     likes: { type: Number, default: 0 },
     views: { type: Number, default: 0 },
     bookmarks: { type: Number, default: 0 },
+    shares: { type: Number, default: 0 },
     comments: { type: [commentSchema], default: [] }
 }, { timestamps: true });
 //here the post is like the collection which store the data which is write in the mongoose.model("post",postSchema);
